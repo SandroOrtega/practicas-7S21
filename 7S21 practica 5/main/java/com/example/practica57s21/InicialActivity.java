198ccf4a2ab04180a3e5052fd0370d2eb1696075
SandroOrtega <109990326+SandroOrtega@users.noreply.github.com>
2022-10-05 18:24:14 -0500

    Add files via upload

---
+++ 7S21 practica 5/main/java/com/example/practica57s21/InicialActivity.java
@@ -0,0 +1,32 @@
+package com.example.practica57s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class InicialActivity extends AppCompatActivity {
+
+    Handler manejador = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+        manejador.postDelayed(autocambiar,5000);
+
+
+    }
+
+    Runnable autocambiar = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(InicialActivity.this,PrincipalActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+    };
+}
